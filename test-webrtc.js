dfe4e5533fc5ecd1a0b030f07c37179ec69e2c2e
yourmomyourdad <103446323+yourmomyourdad@users.noreply.github.com>
2026-08-27 07:41:09 -0500

    Update and rename test-webrtc.js to webrtc.js

--- test-webrtc.js
+++
@@ -1,9 +0,0 @@
-const wrtc = require("@roamhq/wrtc");
-
-console.log("WebRTC loaded!");
-
-console.log({
-    RTCPeerConnection: typeof wrtc.RTCPeerConnection,
-    RTCSessionDescription: typeof wrtc.RTCSessionDescription,
-    RTCIceCandidate: typeof wrtc.RTCIceCandidate
-});
