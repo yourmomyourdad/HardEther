f036baa67f45e8636c3fbda0cb11613f65d9ffd7
yourmomyourdad <103446323+yourmomyourdad@users.noreply.github.com>
2026-09-09 11:31:27 -0500

    Increase video capture resolution to 1920x1080

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ wss.on("error", error => {
 // ─────────────────────────────────────────────
 
 function startVideoCapture(chromeHeight) {
-    const WIDTH = 1280;
-    const HEIGHT = 720;
+    const WIDTH = 1920;
+    const HEIGHT = 1080;
 
     // I420 / YUV420p = 1.5 bytes per pixel
     const FRAME_SIZE = WIDTH * HEIGHT * 3 / 2;
